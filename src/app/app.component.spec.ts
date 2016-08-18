55721019a3522c1d8d38147b49859cee1eda407a
theoriginaljosh <theoriginaljosh@gmail.com>
2016-08-18 16:00:58 -0400

    spacing fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,17 +4,17 @@ import { addProviders, async, inject } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('App: Subular', () => {
-  beforeEach(() => {
-    addProviders([AppComponent]);
-  });
+	beforeEach(() => {
+		addProviders([AppComponent]);
+	});
 
-  it('should create the app',
-    inject([AppComponent], (app: AppComponent) => {
-      expect(app).toBeTruthy();
-    }));
+	it('should create the app',
+		inject([AppComponent], (app: AppComponent) => {
+			expect(app).toBeTruthy();
+		}));
 
-  it('should have as title \'app works!\'',
-    inject([AppComponent], (app: AppComponent) => {
-      expect(app.title).toEqual('app works!');
-    }));
+	it('should have as title \'app works!\'',
+		inject([AppComponent], (app: AppComponent) => {
+			expect(app.title).toEqual('app works!');
+		}));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+	moduleId: module.id,
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	providers: [AuthenticationService]
 })
 export class AppComponent {
-  title = 'app works!';
+	title = 'app works!';
+
+	constructor() {
+	}
 }
