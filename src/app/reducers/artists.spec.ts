6992cc9610dd8b37d7ba4c75cc2d4516ec041c62
Josh Sommer <JoshDSommer@gmail.com>
2016-08-21 18:46:45 -0400

    Artist Reducer
    Artist list reducer

---
+++ src/app/reducers/artists.spec.ts
@@ -0,0 +1,35 @@
+/// <reference path="../../../typings/index.d.ts" />
+import * as _ from 'lodash';
+
+import { REDUCERS_DICTONARY, artists, ARTIST_ACTIONS, IArtist  } from './reducers.index';
+import { addProviders, inject } from '@angular/core/testing';
+
+describe('Artists Reducer', () => {
+	let intialState: IArtist[];
+
+	beforeEach(() => {
+		intialState = [{
+			id: 1,
+			name: 'mockArtist1'
+		},
+		{
+			id: 2,
+			name: 'mockArtist2'
+		}];
+	});
+	it('should add a new artist to the store', () => {
+		let mockArtist: IArtist = {
+			id: 1,
+			name: 'mockArtist'
+		};
+
+		let result = artists(intialState, { type: ARTIST_ACTIONS.ADD_ARTISTS, payload: [mockArtist] });
+		expect(result).toBeTruthy;
+		expect(result).toContain(mockArtist);
+	});
+	it('should get all the artists', () => {
+		let result = artists(intialState, { type: ARTIST_ACTIONS.GET_ARTISTS, payload: null });
+		expect(result).toBeTruthy;
+		expect(result).toBeTruthy();
+	});
+});
---
+++ src/app/reducers/artists.ts
@@ -0,0 +1,24 @@
+
+import { ActionReducer  } from '@ngrx/store';
+
+
+export interface IArtist {
+	id: number;
+	name: string;
+}
+
+export const ARTIST_ACTIONS = {
+	ADD_ARTISTS: 'ADD_ARTISTS',
+	GET_ARTISTS: 'GET_ARTISTS',
+};
+
+export const artists: ActionReducer<IArtist[]> = (state = [], {type, payload}) => {
+	switch (type) {
+		case ARTIST_ACTIONS.ADD_ARTISTS:
+			return state.concat(payload);
+		case ARTIST_ACTIONS.GET_ARTISTS:
+			return state;
+		default:
+			return state;
+	}
+};
