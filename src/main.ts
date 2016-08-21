0399271b7f3558886f8f0e425d54654930dae01c
Josh Sommer <JoshDSommer@gmail.com>
2016-08-21 18:49:08 -0400

    hooking up reducers

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
+/// <reference path="../typings/index.d.ts" />
+
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
-import { provideStore } from '@ngrx/store';
+import { StoreModule, provideStore } from '@ngrx/store';
+import { HTTP_PROVIDERS } from '@angular/http';
 
-import { sample } from './app/reducers/reducers.index';
+import { servers, artists } from './app/reducers/reducers.index';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrap(AppComponent,
-  provideStore({sample})
+  [provideStore({ servers, artists }), HTTP_PROVIDERS]
 );
