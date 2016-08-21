1799d40da5597a36a2a99f481790efe858e8e8e1
Josh Sommer <JoshDSommer@gmail.com>
2016-08-21 18:48:54 -0400

    component that simply just lists out all artists in server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,43 @@
 import { Component } from '@angular/core';
-import { AuthenticationService } from './services/authentication.service';
+import { SubularService } from './services/subsonic.service';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { IServer, IArtist, REDUCERS_DICTONARY, SERVER_ACTIONS, AppState } from './reducers/reducers.index';
 
 @Component({
 	moduleId: module.id,
 	selector: 'app-root',
 	templateUrl: 'app.component.html',
 	styleUrls: ['app.component.css'],
-	providers: [AuthenticationService]
+	providers: [ SubularService]
 })
 export class AppComponent {
 	title = 'app works!';
+	artists: Observable<IArtist>;
+	server: Observable<IServer[]>;
+	appState: Observable<AppState>;
 
-	constructor() {
+	constructor(private subular: SubularService, private store: Store<any>) {
+		this.artists = <any>this.store.select(REDUCERS_DICTONARY.artists);
+		this.server = <any>this.store.select(REDUCERS_DICTONARY.servers);
+		this.appState = <any>this.store.select(REDUCERS_DICTONARY.appState);
+
+		let newServer: IServer = {
+			name: '',
+			serverAddress: '',
+			serverPassword: '',
+			serverUserName: '',
+			salt: '',
+			selected: true
+		}
+
+		this.server.subscribe((server: IServer[]) => {
+			if (server && server[0]) {
+				this.subular.buildServerData(server[0]);
+			}
+		});
+
+		this.store.dispatch({ type: SERVER_ACTIONS.ADD_SERVER, payload: newServer });
+
 	}
 }
