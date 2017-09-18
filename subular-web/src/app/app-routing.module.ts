225dd4913b9f001bea758d35843a66228ac7d66c
theoriginaljosh <theoriginaljosh@gmail.com>
2017-09-18 14:54:30 -0400

    adding routers

--- subular-web/src/app/app-routing.module.ts
+++ subular-web/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule, Injectable } from '@angular/core';
 import { Routes, RouterModule, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, } from '@angular/router';
 import { LoginComponent } from './views/login/login.component'
+import { SubularAppComponent } from './views/subular-app/subular-app.component';
+import { RandomAlbumsComponent } from './views/random-albums/random-albums.component';
 
 const routes: Routes = [
-	{ path: '', redirectTo: '/login', pathMatch: 'full' },
 	{ path: 'login', component: LoginComponent },
+	{
+		path: 'app', component: SubularAppComponent, children: [
+			{ path: '', component: RandomAlbumsComponent, outlet: 'subular' }
+		]
+	},
 
 ];
 
