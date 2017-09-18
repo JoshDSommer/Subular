69c25315490a29d8ea7f82c6e2813fe8e5c1eeee
theoriginaljosh <theoriginaljosh@gmail.com>
2017-09-18 12:54:32 -0400

    tieing al this login stuff together.

--- subular-web/src/app/app.module.ts
+++ subular-web/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { InputTextModule, PasswordModule, ButtonModule } from 'primeng/primeng';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { SubularBrandComponent } from './components/subular-brand/subular-brand.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SubularBrandComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
