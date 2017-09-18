71911df59be1c79028b16bf2104483393ddf74b0
Josh Sommer <JoshDSommer@gmail.com>
2017-09-17 21:56:13 -0400

    adding first shared component to {N}

--- subular-mobile/app/app.component.ts
+++ subular-mobile/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
+import { SubsonicAuthenticationService } from 'subular';
 
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html",
 })
 
-export class AppComponent { }
+export class AppComponent {
+    constructor(private subsonicAuthenticationService: SubsonicAuthenticationService) {
+        console.log(subsonicAuthenticationService.test);
+    }
+}
