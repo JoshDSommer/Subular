b1692389145a7506ae6143adf649b785d82f061a
Josh Sommer <JoshDSommer@gmail.com>
2017-09-17 21:47:01 -0400

    adding first shared service with hello world.

---
+++ subular-web/src/shared-services/index.ts
@@ -0,0 +1,1 @@
+export * from './subsonic-authentication.service';
---
+++ subular-web/src/shared-services/subsonic-authentication.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class SubsonicAuthenticationService {
+
+	public test = 'Hello World';
+	constructor() { }
+}
