40c2a86e5fad92aaca960c8c040e1d8a31827897
theoriginaljosh <theoriginaljosh@gmail.com>
2017-09-18 14:54:17 -0400

    adding subular main app component.
    Main subular App.

---
+++ subular-web/src/app/views/subular-app/subular-app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'subular-app',
+	templateUrl: 'subular-app.component.html'
+})
+
+export class SubularAppComponent implements OnInit {
+
+	ngOnInit() { }
+}
