552582672b4a60d362567a5b66c7ec285d201239
theoriginaljosh <theoriginaljosh@gmail.com>
2017-09-19 15:27:31 -0400

    adding more fun stuff to the index

--- subular-web/src/shared-services/index.ts
+++ subular-web/src/shared-services/index.ts
@@ -1,3 +1,6 @@
 export * from './subsonic-authentication.service';
 export * from './localstorage.provider';
 export * from './subsonic.service';
+export * from './subsonic.guard';
+export * from './cached-album-artist.service';
+export * from './interfaces';
