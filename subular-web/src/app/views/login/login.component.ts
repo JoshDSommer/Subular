78708fc0fe52218c6bda5e17d68fdf140263873a
theoriginaljosh <theoriginaljosh@gmail.com>
2017-09-19 11:59:45 -0400

    typo in comment

--- subular-web/src/app/views/login/login.component.ts
+++ subular-web/src/app/views/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent implements OnInit {
 				this.router.navigate(['/app'])
 			}
 		}, failed => {
-			// todo replae with something like ngx-toastr
+			// todo replace with something like ngx-toastr
 			window.alert('Login failed');
 		});
 
