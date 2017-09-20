45959969580d568ef1a627bf587bdf9f2dec92d1
theoriginaljosh <theoriginaljosh@gmail.com>
2017-09-20 14:18:04 -0400

    adding functions for builing artists
    also the refresh is for the instiall load.

--- subular-web/src/shared-services/cached-album-artist.service.ts
+++ subular-web/src/shared-services/cached-album-artist.service.ts
@@ -8,6 +8,7 @@ import { LOCALSTORAGE_PROVIDER } from './localstorage.provider';
 
 
 const SUBULAR_CACHED_ALBUMS = 'subular.cached.albums';
+const SUBULAR_CACHED_ARTISTS = 'subular.cached.artists';
 
 @Injectable()
 export class CachedAlbumArtistService {
@@ -18,20 +19,36 @@ export class CachedAlbumArtistService {
 
 	}
 
+
+
 	refreshedCachedData(): Observable<boolean> {
-		return this.buildAlbumDatabase();
+		return this.getArtists()
+			.switchMap(() => this.getAlbums())
+			.switchMap(() => Observable.of(true));
 	}
 
-	getAlbums():Observable<IAlbum[]>{
+	getArtists(): Observable<IArtist[]> {
+		const artists = this.localStorage.getValue(SUBULAR_CACHED_ARTISTS);
+		if (artists) {
+			return new Observable(observer => {
+				observer.next(artists);
+				observer.complete();
+			});
+		}
+		// else rebuild albumDB  and then get it form local storage.
+		return this.buildArtistDatabase().switchMap(() => this.getArtists());
+	}
+
+	getAlbums(): Observable<IAlbum[]> {
 		const albums = this.localStorage.getValue(SUBULAR_CACHED_ALBUMS);
-		if(albums){
-			return new Observable(observer =>{
+		if (albums) {
+			return new Observable(observer => {
 				observer.next(albums);
 				observer.complete();
 			});
 		}
 		// else rebuild albumDB  and then get it form local storage.
-		return this.buildAlbumDatabase().switchMap(()=>this.getAlbums());
+		return this.buildAlbumDatabase().switchMap(() => this.getAlbums());
 	}
 
 
@@ -43,15 +60,24 @@ export class CachedAlbumArtistService {
 	private buildAlbumDatabase(albums: IAlbum[] = [], offset: number = 0): Observable<boolean> {
 		return this.subsonic
 			.subsonicGet('getAlbumList', '&type=newest&size=500&offset=' + offset)
+			.map(data => data.subresp.albumList.album as IAlbum[])
 			.switchMap(data => {
-				let newAlbums = data.subresp.albumList.album as IAlbum[];
-				albums = [...albums, ...newAlbums,]
-				if (newAlbums.length === 500) {
+				albums = [...albums, ...data,]
+				if (data.length === 500) {
 					return this.buildAlbumDatabase(albums, offset + 500);
 				} else {
 					this.localStorage.setValue(SUBULAR_CACHED_ALBUMS, albums);
 					return Observable.of(true);
 				}
 			});
+	}
+
+	private buildArtistDatabase(): Observable<any> {
+		return this.subsonic.subsonicGet('getIndexes')
+			.map(data => data.subresp.indexes.index
+				.map(value => value.artist)
+				.reduce((previous, value) => [...previous, ...value]) as IArtist[]
+			)
+			.do(artists => this.localStorage.setValue(SUBULAR_CACHED_ARTISTS, artists))
 	}
 }
