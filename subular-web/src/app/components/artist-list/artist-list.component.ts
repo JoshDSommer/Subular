8061fdbf7a5552fa53b5698440b623d088dc42df
theoriginaljosh <theoriginaljosh@gmail.com>
2017-09-21 12:43:15 -0400

    refactoring component to allow juming to an item.

--- subular-web/src/app/components/artist-list/artist-list.component.ts
+++ subular-web/src/app/components/artist-list/artist-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { IArtist } from '../../../shared-services/index';
+
 
 @Component({
 	selector: 'artist-list',
@@ -9,6 +10,38 @@ import { IArtist } from '../../../shared-services/index';
 })
 export class ArtistListComponent implements OnInit {
 	@Input() artists: IArtist[];
+	@ViewChild('artistList') artistListUL: ElementRef;
+
+	@HostListener('window:keyup', ['$event'])
+	keyEvent(event: KeyboardEvent) {
+		if (this.artists) {
+			const artist = this.searchArtistsWithDebounce(event.key);
+		}
+	}
+
+	private searchValue = '';
+	private timeOut;
+	searchArtistsWithDebounce(value) {
+		clearTimeout(this.timeOut);
+		this.searchValue = this.searchValue + value;
+		this.timeOut = setTimeout(() => {
+			const artist = this.getFirstArtistThatStartsWith(this.searchValue);
+			this.scrollToArtist(artist);
+			this.searchValue = '';
+		}, 300);
+	}
+
+	scrollToArtist(artist: IArtist) {
+		if (artist) {
+			const nativeHtmlUL = this.artistListUL.nativeElement as HTMLUListElement;
+			const artistLI = nativeHtmlUL.querySelectorAll(`[class='${artist.id}']`)[0] as HTMLLIElement;
+			nativeHtmlUL.scrollTop = artistLI.offsetTop - 25;
+		}
+	}
+
+	getFirstArtistThatStartsWith(startsWith: string): IArtist {
+		return this.artists.find(value => value.name.substr(0, startsWith.length).toLowerCase() == startsWith.toLowerCase());
+	}
 
 	ngOnInit() { }
 
