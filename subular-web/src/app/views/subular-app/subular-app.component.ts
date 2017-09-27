4691a8252c9e092f3eb4ff2fd3560099851e3181
theoriginaljosh <theoriginaljosh@gmail.com>
2017-09-27 11:12:32 -0400

    refactoring subular app to use angular's async pipe and to push selected artistid down to the artist list.

--- subular-web/src/app/views/subular-app/subular-app.component.ts
+++ subular-web/src/app/views/subular-app/subular-app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SubsonicCachedService } from '../../../shared-services/subsonic.cached.service';
 import { Observable } from 'rxjs/Observable';
 import { IAlbum, IArtist } from '../../../shared-services/index';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 @Component({
 	selector: 'subular-app',
@@ -10,18 +11,20 @@ import { IAlbum, IArtist } from '../../../shared-services/index';
 })
 
 export class SubularAppComponent implements OnInit {
-	albums: IAlbum[];
-	artists: IArtist[];
+	artists$: Observable<IArtist[]>;
 	loaded: boolean;
+	artistId$: Observable<any>;
 
-	constructor(private cachedData: SubsonicCachedService) {
+	constructor(private cachedData: SubsonicCachedService, private route: ActivatedRoute, private router: Router) {}
 
-	}
 	ngOnInit() {
-		this.cachedData.getCachedData().subscribe(([artists,albums]) => {
-			this.artists = artists;
-			this.albums = albums;
-			this.loaded = true;
-		});
+		this.artists$ = this.cachedData.getCachedData()
+			.map(([artists, albums]) => artists)
+			.do(() => this.loaded = true);
+
+		this.artistId$ = Observable.of(this.route.firstChild.snapshot.params['artistId'])
+			.merge(this.router.events
+				.filter(value => value instanceof NavigationEnd)
+				.map(() => +this.route.firstChild.snapshot.params['artistId']));
 	}
 }
