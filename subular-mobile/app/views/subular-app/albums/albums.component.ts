9999988e78b794da63e0725937b8ae5ee35a5aa8
Josh Sommer <JoshDSommer@gmail.com>
2017-09-29 16:02:40 -0400

    basic album component functionality

---
+++ subular-mobile/app/views/subular-app/albums/albums.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { IAlbum } from 'subular';
+import { RouterExtensions } from 'nativescript-angular/router';
+
+@Component({
+	moduleId: module.id,
+	selector: 'albums',
+	templateUrl: 'albums.component.html',
+	styleUrls: ['albums.component.css']
+})
+
+export class AlbumsComponent implements OnInit {
+	albums: IAlbum[];
+	constructor(private route: ActivatedRoute, private router: Router, private nsRouter: RouterExtensions) {
+
+	}
+	back() {
+		this.nsRouter.back();
+	}
+	ngOnInit() {
+		this.albums = this.route.snapshot.data['albums'] as IAlbum[];
+		this.router.events.subscribe(() => {
+			this.albums = this.route.snapshot.data['albums'] as IAlbum[];
+		});
+	}
+}
