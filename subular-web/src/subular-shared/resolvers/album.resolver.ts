98882a512d323e5b945689c5eeb10a85c5389b99
theoriginaljosh <theoriginaljosh@gmail.com>
2017-10-05 08:49:38 -0400

    adding album resolver

---
+++ subular-web/src/subular-shared/resolvers/album.resolver.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { IAlbum } from '../interfaces/album';
+import { SubsonicCachedService } from '../subsonic.cached.service';
+
+
+@Injectable()
+export class AlbumResolver implements Resolve<IAlbum> {
+
+	constructor(private cache: SubsonicCachedService) {
+	}
+
+	resolve(route: ActivatedRouteSnapshot): Observable<IAlbum> {
+		return this.cache.getAlbums()
+			.map(albums => {
+				const albumId = +route.paramMap.get('albumId');
+				const filtered = albums.find(album => album.id == albumId)
+				return filtered;
+			});
+	}
+}
--- subular-web/src/subular-shared/resolvers/index.ts
+++ subular-web/src/subular-shared/resolvers/index.ts
@@ -1,1 +1,2 @@
 export * from './albums.resolver';
+export * from './album.resolver';
