de57b3420a556328e7412fc1dbaef2e7933e0ac5
theoriginaljosh <theoriginaljosh@gmail.com>
2017-11-08 15:55:43 -0500

    updating album.component to work with player service

--- subular-web/src/app/views/subular-app/album/album.component.ts
+++ subular-web/src/app/views/subular-app/album/album.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterResolverDataObservable, IAlbum, SubsonicService, ISong } from '../../../../subular-shared';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { PlayerService } from '../../../services/player.service';
 
 @Component({
 	selector: 'album',
@@ -10,14 +11,20 @@ import { Observable } from 'rxjs';
 })
 
 export class AlbumComponent implements OnInit {
-	songs$: Observable<ISong[]>
+	songs$: Observable<ISong[]>;
 	album$: Observable<IAlbum>;
 
-	dataTableSongs: ISong[] =[];
+	dataTableSongs: ISong[] = [];
 
-	constructor(private route: ActivatedRoute, private router: Router, private subsonic: SubsonicService) { }
+	constructor(private route: ActivatedRoute,
+		private router: Router,
+		private subsonic: SubsonicService,
+		private playerService: PlayerService) { }
 	ngOnInit() {
 		this.album$ = RouterResolverDataObservable<IAlbum>(this.route, this.router, 'album');
 		this.songs$ = this.album$.switchMap(album => this.subsonic.getSongs(album.id));
+	}
+	selectSong($song: ISong) {
+		this.playerService.addAndPlaySong($song);
 	}
 }
