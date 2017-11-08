87eeafcb327e1b270493a000b5d89ca43d5ff34e
theoriginaljosh <theoriginaljosh@gmail.com>
2017-11-08 07:47:17 -0500

    getting rid of overloads
    really servers no purpose

--- subular-web/src/subular-shared/services/subsonic.service.ts
+++ subular-web/src/subular-shared/services/subsonic.service.ts
@@ -27,11 +27,9 @@ export class SubsonicService {
 		return this.authentication.getServerURl('getCoverArt') + `&id=${id}&size=274`;
 	}
 
-	subsonicGet(method: string);
-	subsonicGet(method: string, additionalParams: string);
 	subsonicGet(method: string, additionalParams?: string) {
 		const url = additionalParams ? this.authentication.getServerURl(method) + additionalParams : this.authentication.getServerURl(method);
-		if (url == '' || url == additionalParams) {
+		if (url === '' || url === additionalParams) {
 			return Observable.of(false);
 		}
 
