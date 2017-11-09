217a9b3a5876a6d86a800479a28b2a2cab546e88
theoriginaljosh <theoriginaljosh@gmail.com>
2017-11-09 14:24:30 -0500

    album info component work

--- subular-web/src/app/views/subular-app/album/album.component.ts
+++ subular-web/src/app/views/subular-app/album/album.component.ts
@@ -3,6 +3,7 @@ import { RouterResolverDataObservable, IAlbum, SubsonicService, ISong } from '..
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PlayerService } from '../../../services/player.service';
+import { MenuItem } from 'primeng/primeng';
 
 @Component({
 	selector: 'album',
@@ -11,20 +12,35 @@ import { PlayerService } from '../../../services/player.service';
 })
 
 export class AlbumComponent implements OnInit {
+	contextMenuItems: MenuItem[];
+	nowPlayingSong$ = new Observable<ISong>();
 	songs$: Observable<ISong[]>;
 	album$: Observable<IAlbum>;
 
 	dataTableSongs: ISong[] = [];
 
+	private listedSongs: ISong[];
 	constructor(private route: ActivatedRoute,
 		private router: Router,
 		private subsonic: SubsonicService,
 		private playerService: PlayerService) { }
+
 	ngOnInit() {
 		this.album$ = RouterResolverDataObservable<IAlbum>(this.route, this.router, 'album');
-		this.songs$ = this.album$.switchMap(album => this.subsonic.getSongs(album.id));
+		this.songs$ = this.album$.switchMap(album => this.subsonic.getSongs(album.id))
+			.do(songs => this.listedSongs = songs);
+		this.nowPlayingSong$ = this.playerService.nowPlaying$
+			.filter(nowPlaying => !!nowPlaying && !!nowPlaying.song)
+			.map(nowPlaying => nowPlaying.song);
+
+		this.contextMenuItems = [{
+			label: 'Coming soon...'
+		}];
 	}
+
 	selectSong($song: ISong) {
-		this.playerService.addAndPlaySong($song);
+		this.playerService.clearSongs();
+
+		this.playerService.addSongsAndPlaySong(this.listedSongs, $song);
 	}
 }
