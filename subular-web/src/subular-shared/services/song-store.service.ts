069195a129e7ce8eddaf9910ba74d05780503d4e
Josh Sommer <JoshDSommer@gmail.com>
2017-11-18 11:35:08 -0500

    fiixing a broke path when shared

--- subular-web/src/subular-shared/services/song-store.service.ts
+++ subular-web/src/subular-shared/services/song-store.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
-import { ISong } from '../../subular-shared/index';
+import { ISong } from '../interfaces';
 
 @Injectable()
 export class SongStoreService {
