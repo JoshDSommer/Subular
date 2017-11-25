0c28bfa08f16fc3bdaecca7a87b0d61561cbc34f
Josh Sommer <JoshDSommer@gmail.com>
2017-11-25 13:02:00 -0500

    switching out side drawer for bottom navigation

--- subular-mobile/app/views/subular-app/subular-app.component.ts
+++ subular-mobile/app/views/subular-app/subular-app.component.ts
@@ -1,34 +1,36 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { SubularAppBaseComponent, SubsonicCachedService, SubsonicService } from 'subular';
 import { RadSideDrawer } from "nativescript-pro-ui/sidedrawer";
 import { RadSideDrawerComponent } from "nativescript-pro-ui/sidedrawer/angular";
 import { PlayerService, IAudioPlayingInfo, PlayingStatus } from '../../services/player.service';
+import { GridLayout } from 'ui/layouts/grid-layout';
 
 @Component({
 	moduleId: module.id,
 	selector: 'subular-app',
 	templateUrl: 'subular-app.component.html',
 	styleUrls: ['subular-app.component.css'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class SubularAppComponent extends SubularAppBaseComponent {
 	nowPlaying: IAudioPlayingInfo;
-	// nowPlaying$: Observable<IAudioPlayingInfo>;
 	PlayingStatus = PlayingStatus;
-	@ViewChild(RadSideDrawerComponent) public drawerComponent: RadSideDrawerComponent;
-
-	get drawer(): RadSideDrawer {
-		return this.drawerComponent.sideDrawer;
-	}
+	@ViewChild('mainLayout') mainLayout: ElementRef
 
 	constructor(cachedData: SubsonicCachedService, route: ActivatedRoute, router: Router,
 		public subsonic: SubsonicService,
-		private player: PlayerService) {
+		private player: PlayerService,
+		private ref: ChangeDetectorRef) {
 		super(cachedData, route, router);
-		this.player.nowPlaying$.subscribe(nowPlaying => this.nowPlaying = nowPlaying);
-
+		this.player.nowPlaying$.subscribe(nowPlaying => {
+			if (nowPlaying) {
+				this.nowPlaying = Object.assign({}, nowPlaying);
+				this.ref.markForCheck();
+			}
+		});
 	}
 
 	getArtWork(coverArt) {
