8ee62828b8f77f70fda7d2d7404c35a0699f7262
Josh Sommer <JoshDSommer@gmail.com>
2017-11-27 17:46:06 -0500

    restructing album component to include songs

--- subular-mobile/app/views/subular-app/album/album.component.ts
+++ subular-mobile/app/views/subular-app/album/album.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs';
 import { SLIDE_RIGHT_ANIMATION } from '../../../animations/animations';
+import { PlayerService } from '../../../services/player.service';
 
 @Component({
 	moduleId: module.id,
@@ -15,7 +16,7 @@ import { SLIDE_RIGHT_ANIMATION } from '../../../animations/animations';
 export class AlbumComponent implements OnInit {
 	songSubscription: Subscription;
 	songs$: Observable<ISong[]>;
-	listedSongs: {};
+	listedSongs = [];
 	album$: Observable<IAlbum>;
 	albums: IAlbum;
 
@@ -24,32 +25,36 @@ export class AlbumComponent implements OnInit {
 	constructor(private route: ActivatedRoute,
 		private router: Router,
 		private subsonic: SubsonicService,
-		// private playerService: PlayerService,
+		private playerService: PlayerService,
 		private songStore: SongStoreService) { }
 
 	ngOnInit() {
 		this.album$ = RouterResolverDataObservable<IAlbum>(this.route, this.router, 'album');
-				// this.songs$ =
-		this.songSubscription = this.album$.switchMap(album => this.subsonic.getSongs(album.id))
+		// this.songs$ =
+		this.songs$ = this.album$.switchMap(album => this.subsonic.getSongs(album.id))
 			// this map is to filter out duplicates.
 			.map(songs => songs.filter((song, index, self) => {
 				return index === self.findIndex((previosSong) => {
 					return previosSong.title === song.title && previosSong.track === song.track;
 				});
 			}))
-			.switchMap(songs => this.songStore.addSongs(songs)).subscribe();
+			.switchMap(songs => this.songStore.addSongs(songs))
+			.do(songs => this.listedSongs = songs);
 		// 	.do(songs => this.listedSongs = songs);
 		// this.nowPlayingSong$ = this.playerService.nowPlaying$
 		// .filter(nowPlaying => !!nowPlaying && !!nowPlaying.song)
 		// .map(nowPlaying => nowPlaying.song);
 
 	}
-	playingSong() {
-		console.log('playingSong')
+
+	selectSong($song: ISong) {
+		this.playerService.clearSongs();
+
+		this.playerService.addSongsAndPlaySong(this.listedSongs, $song);
 	}
+
 	ngOnDestroy() {
 		//Called once, before the instance is destroyed.
 		//Add 'implements OnDestroy' to the class.
-		this.songSubscription.unsubscribe();
 	}
 }
