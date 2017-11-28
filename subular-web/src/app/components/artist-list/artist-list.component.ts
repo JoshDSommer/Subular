6a93caa5a53b3fc1addbff3f29ecb5ae45cea3de
theoriginaljosh <theoriginaljosh@gmail.com>
2017-11-28 10:22:42 -0500

    refacorting out typing to directive.

--- subular-web/src/app/components/artist-list/artist-list.component.ts
+++ subular-web/src/app/components/artist-list/artist-list.component.ts
@@ -1,57 +1,22 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { IArtist, SubsonicCachedService } from '../../../subular-shared/index';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 
 @Component({
 	selector: 'artist-list',
 	templateUrl: 'artist-list.component.html',
 	styleUrls: ['artist-list.component.css'],
-	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ArtistListComponent implements OnInit {
+	artistId$: Observable<any>;
 	artists$: any;
 	@Input() selectedArtistId: number;
 	@Input() artists: IArtist[];
-	@ViewChild('artistList') artistListUL: ElementRef;
 
-	@HostListener('window:keyup', ['$event'])
-	keyEvent(event: KeyboardEvent) {
-		if (this.artists) {
-			const artist = this.searchArtistsWithDebounce(event.key);
-		}
-	}
 
-	private searchValue = '';
-	private timeOut;
-
-	constructor(private cachedData: SubsonicCachedService) { }
-
-	searchArtistsWithDebounce(value) {
-		clearTimeout(this.timeOut);
-		this.searchValue = this.searchValue + value;
-		this.timeOut = setTimeout(() => {
-			const artist = this.getFirstArtistThatStartsWith(this.searchValue);
-			this.scrollToArtist(artist);
-			this.searchValue = '';
-		}, 500);
-	}
-
-	scrollToArtist(artist: IArtist) {
-		if (artist) {
-			const nativeHtmlUL = this.artistListUL.nativeElement as HTMLUListElement;
-			const artistLI = nativeHtmlUL.querySelector(`[class='${artist.id}']`) as HTMLLIElement;
-			nativeHtmlUL.scrollTop = artistLI.offsetTop - 100;
-			artistLI.querySelector('a').click()
-		}
-	}
-
-	getFirstArtistThatStartsWith(startsWith: string): IArtist {
-		return this.artists.find(value => value.name
-			.toLowerCase()
-			.replace('the ', '')
-			.substr(0, startsWith.length) == startsWith.toLowerCase()
-		);
-	}
+	constructor(private cachedData: SubsonicCachedService, private route: ActivatedRoute, private router: Router) { }
 
 	ngOnInit() {
 		this.artists$ = this.cachedData.getCachedData()
