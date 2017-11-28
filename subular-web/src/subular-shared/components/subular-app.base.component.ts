a57faa8bd357d3eea6b0dd982dd4188b261f9ead
theoriginaljosh <theoriginaljosh@gmail.com>
2017-11-28 10:23:26 -0500

    refactoring observable to use startWith
    much cleaner than the roud about merging.

--- subular-web/src/subular-shared/components/subular-app.base.component.ts
+++ subular-web/src/subular-shared/components/subular-app.base.component.ts
@@ -5,24 +5,24 @@ import { SubsonicCachedService } from '../services/subsonic.cached.service';
 import { IArtist } from '../interfaces/artists';
 
 @Component({
-	template:''
+	template: ''
 })
 export class SubularAppBaseComponent implements OnInit {
 	loaded$: Observable<boolean>;
 	artistId$: Observable<any>;
 
-	constructor(private cachedData: SubsonicCachedService, private route: ActivatedRoute, private router: Router) {}
+	constructor(private cachedData: SubsonicCachedService, private route: ActivatedRoute, private router: Router) { }
 
 	ngOnInit() {
 		this.loaded$ = this.cachedData.getCachedData()
 			.map(([artists, albums]) => true);
 
 
-		if (this.route && this.route.firstChild && this.route.firstChild.snapshot  && this.route.firstChild.snapshot.params) {
-			this.artistId$ = Observable.of(this.route.firstChild.snapshot.params['artistId'])
-			.merge(this.router.events
+		if (this.route && this.route.firstChild && this.route.firstChild.snapshot && this.route.firstChild.snapshot.params) {
+			this.artistId$ = this.router.events
 				.filter(value => value instanceof NavigationEnd)
-				.map(() => this.route.firstChild.snapshot.params['artistId']));
+				.map(() => this.route.firstChild.snapshot.params['artistId'])
+				.startWith(this.route.firstChild.snapshot.params['artistId']);
 		}
 
 	}
--- subular-web/src/subular-shared/functions.ts
+++ subular-web/src/subular-shared/functions.ts
@@ -4,20 +4,20 @@ import { NavigationEnd, ActivatedRoute, Router } from "@angular/router";
 
 export function RouterParamObservable<T>(route: ActivatedRoute, router: Router, paramName: string): Observable<T> {
 	if (route && route.snapshot && route.snapshot.params) {
-		return Observable.of(route.snapshot.params[paramName])
-			.merge(router.events
+		return router.events
 				.filter(value => value instanceof NavigationEnd)
-				.map(() => route.snapshot.params[paramName]));
+				.map(() => route.snapshot.params[paramName])
+				.startWith(route.snapshot.params[paramName]);
 	}
 	return Observable.empty();
 }
 
 
 export function RouterResolverDataObservable<T>(route: ActivatedRoute, router: Router, paramName: string): Observable<T> {
-	//	if (route && route.firstChild && route.firstChild.snapshot && route.firstChild.snapshot.params) {
-	return Observable.of(route.snapshot.data[paramName])
-		.merge(router.events
+	// if (route && route.firstChild && route.firstChild.snapshot && route.firstChild.snapshot.params) {
+	return router.events
 			.filter(value => value instanceof NavigationEnd)
-			.map(() => route.snapshot.data[paramName]));
+			.map(() => route.snapshot.data[paramName])
+			.startWith(route.snapshot.data[paramName]);
 	//
 }
