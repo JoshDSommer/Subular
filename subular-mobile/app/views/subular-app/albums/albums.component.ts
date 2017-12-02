2b58a6262d378986eed337b1e84f6deb34d771a5
Josh Sommer <JoshDSommer@gmail.com>
2017-12-02 08:38:30 -0500

    Styling Albums componet

--- subular-mobile/app/views/subular-app/albums/albums.component.ts
+++ subular-mobile/app/views/subular-app/albums/albums.component.ts
@@ -4,6 +4,7 @@ import { IAlbum } from 'subular';
 import { RouterExtensions } from 'nativescript-angular/router';
 import { SLIDE_RIGHT_ANIMATION } from '../../../animations/animations';
 import { SubularMobileService } from '../../../services/subularMobile.service';
+import { screen } from 'platform';
 
 
 @Component({
@@ -17,6 +18,8 @@ export class AlbumsComponent implements OnInit {
 	albums: IAlbum[];
 
 	animateOptions = SLIDE_RIGHT_ANIMATION
+	imageHeightWidth = (screen.mainScreen.widthDIPs / 12) * 5;
+	imageSideMargins = (screen.mainScreen.widthDIPs / 18);
 
 	constructor(private route: ActivatedRoute,
 		private router: Router,
