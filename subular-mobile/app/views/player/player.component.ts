f9dfc363d89bca820cec0be6cd6a2ed531e1e08e
Josh Sommer <JoshDSommer@gmail.com>
2017-12-02 22:30:50 -0500

    changin on touch background color

--- subular-mobile/app/views/player/player.component.ts
+++ subular-mobile/app/views/player/player.component.ts
@@ -21,7 +21,7 @@ import { ActionBar } from 'tns-core-modules/ui/action-bar/action-bar';
 
 export class PlayerComponent implements OnInit {
 	timeEclipsed: string;
-	highlightBgColor = '#ca9fdb';
+	highlightBgColor = '#ebd2f5';
 	private subscription: Subscription;
 	nowPlaying: IAudioPlayingInfo;
 
