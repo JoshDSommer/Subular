db16b4353e884510b5e73fdf9c2c20a2b42aeda9
Josh Sommer <JoshDSommer@gmail.com>
2018-01-13 22:35:39 -0500

    fixing import of directive that doesnt' exist

--- subular-mobile/app/app.module.ts
+++ subular-mobile/app/app.module.ts
@@ -37,7 +37,6 @@ import { PlaylistComponent } from './views/subular-app/playlist/playlist.compone
 import { SlideDownBackDirective } from './directives/slideDownBack.directive';
 import { CurrentConnectionService } from './services/currentConnection.service';
 import { RecentlyAddedComponent } from './views/subular-app/recently-added/recently-added.component';
-import { LongPressDirective } from './directives/longPress.directive';
 
 if (applicationModule.android) {
     applicationModule.on("launch", () => {
@@ -75,7 +74,6 @@ if (applicationModule.android) {
         PlaylistComponent,
         SlideDownBackDirective,
         RecentlyAddedComponent,
-        LongPressDirective,
     ],
     providers: [
         LOCALSTORAGE_SERVICE,
