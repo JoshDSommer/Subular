bde3e00c6dde1d4b38ecf7a3aafc629a4f6ceeb3
Josh Sommer <JoshDSommer@gmail.com>
2018-07-04 09:23:08 -0400

    adding a nativescript app

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Subular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Subular';
+}
