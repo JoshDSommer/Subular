27b20943753dc5e4db5be06e013f56179e3b6b34
Josh Sommer <JoshDSommer@gmail.com>
2018-07-04 15:40:36 -0400

    updating song list & play list

--- apps/mobile/app/components/song-list/song-list.component.ts
+++ apps/mobile/app/components/song-list/song-list.component.ts
@@ -1,33 +1,42 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Output,
+  EventEmitter
+} from '@angular/core';
 import { SongStoreService, ISong } from '@Subular/core';
 import { Observable } from 'rxjs/Observable';
 import { PlayerService } from '../../services/player.service';
 
 @Component({
-	moduleId: module.id,
-	selector: 'song-list',
-	templateUrl: './song-list.component.html',
-	styleUrls: ['./song-list.component.css'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+  moduleId: module.id,
+  selector: 'song-list',
+  templateUrl: './song-list.component.html',
+  styleUrls: ['./song-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-
 export class SongListComponent implements OnInit {
-	@Output() playingSong = new EventEmitter<ISong>();
-	listedSongs: ISong[];
-	songs$: Observable<ISong[]>
+  @Output() playingSong = new EventEmitter<ISong>();
+  listedSongs: ISong[];
+  songs$: Observable<ISong[]>;
 
+  constructor(
+    private songStore: SongStoreService,
+    private playerService: PlayerService
+  ) {}
 
-	constructor(private songStore: SongStoreService, private playerService: PlayerService) { }
+  ngOnInit() {
+    this.songs$ = this.songStore.songs$.pipe(
+      tap(songs => (this.listedSongs = songs))
+    );
+  }
 
-	ngOnInit() {
-		this.songs$ = this.songStore.songs$
-			.do(songs => this.listedSongs = songs);
-	}
+  selectSong($song: ISong) {
+    this.playerService.clearSongs();
 
-	selectSong($song: ISong) {
-		this.playerService.clearSongs();
-
-		this.playerService.addSongsAndPlaySong(this.listedSongs, $song);
-		this.playingSong.emit($song);
-	}
+    this.playerService.addSongsAndPlaySong(this.listedSongs, $song);
+    this.playingSong.emit($song);
+  }
 }
