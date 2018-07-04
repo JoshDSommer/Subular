e9163558425d00cd1427feb4adbb73612ac9773e
Josh Sommer <JoshDSommer@gmail.com>
2018-07-04 15:43:02 -0400

    updating song list

--- apps/mobile/app/components/song-list/song-list.component.ts
+++ apps/mobile/app/components/song-list/song-list.component.ts
@@ -2,13 +2,13 @@ import {
   Component,
   OnInit,
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Output,
   EventEmitter
 } from '@angular/core';
 import { SongStoreService, ISong } from '@Subular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { PlayerService } from '../../services/player.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   moduleId: module.id,
