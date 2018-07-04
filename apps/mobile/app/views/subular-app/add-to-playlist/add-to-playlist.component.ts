bb4f487ee6ce8b3d7fd6d5844e2b831e99ec2a5c
Josh Sommer <JoshDSommer@gmail.com>
2018-07-04 17:49:33 -0400

    fixing rxjs

--- apps/mobile/app/views/subular-app/add-to-playlist/add-to-playlist.component.ts
+++ apps/mobile/app/views/subular-app/add-to-playlist/add-to-playlist.component.ts
@@ -2,38 +2,49 @@ import { Component, OnInit } from '@angular/core';
 import { SubularMobileService } from '../../../services';
 import { Observable } from 'rxjs/Observable';
 import { IPlaylists, IPlaylist } from '@Subular/core';
-import { ActivatedRouteSnapshot, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { RouterExtensions } from 'nativescript-angular/router';
+import { map, tap, switchMap } from 'rxjs/operators';
 
 @Component({
-	moduleId: module.id,
-	selector: 'add-to-playlist',
-	templateUrl: './add-to-playlist.component.html',
-	styleUrls: ['./add-to-playlist.component.css']
+  moduleId: module.id,
+  selector: 'add-to-playlist',
+  templateUrl: './add-to-playlist.component.html',
+  styleUrls: ['./add-to-playlist.component.css']
 })
 export class AddToPlaylistComponent implements OnInit {
-	playlists$: Observable<IPlaylists>;
+  playlists$: Observable<IPlaylists>;
 
-	constructor(private subular: SubularMobileService, private route: ActivatedRoute, private nsRouter : RouterExtensions) { }
+  constructor(
+    private subular: SubularMobileService,
+    private route: ActivatedRoute,
+    private nsRouter: RouterExtensions
+  ) {}
 
-	ngOnInit() {
-		this.playlists$ = this.subular.getPlaylists()
-			.map(playlists => [{ id: 0, name: 'Favorites' } as IPlaylist, ...playlists])
-	}
+  ngOnInit() {
+    this.playlists$ = this.subular
+      .getPlaylists()
+      .map(playlists => [
+        { id: 0, name: 'Favorites' } as IPlaylist,
+        ...playlists
+      ]);
+  }
 
-	getCoverArt(playlist: IPlaylist) {
-		return this.subular.subsonicGetPlaylistCoverUrl(playlist);
-	}
+  getCoverArt(playlist: IPlaylist) {
+    return this.subular.subsonicGetPlaylistCoverUrl(playlist);
+  }
 
-	addToPlaylist(playlist: IPlaylist) {
-		this.route.params
-			.map(params => params['songId'])
-			.do(console.log)
-			.switchMap(songId => {
-				return this.subular.addToPlaylist({ id: songId } as any, playlist.id);
-			})
-			.subscribe(() => {
-				this.nsRouter.back();
-			});
-	}
+  addToPlaylist(playlist: IPlaylist) {
+    this.route.params
+      .pipe(
+        map(params => params['songId']),
+        tap(console.log),
+        switchMap(songId => {
+          return this.subular.addToPlaylist({ id: songId } as any, playlist.id);
+        })
+      )
+      .subscribe(() => {
+        this.nsRouter.back();
+      });
+  }
 }
