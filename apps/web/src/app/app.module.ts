746caea3828ca316254cf7f5cc50d2a88f50884a
Josh Sommer <JoshDSommer@gmail.com>
2018-07-04 11:08:07 -0400

    adding subular to main module

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
+import { SubularCoreModule } from '@Subular/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot()],
+  imports: [BrowserModule, NxModule.forRoot(), SubularCoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
