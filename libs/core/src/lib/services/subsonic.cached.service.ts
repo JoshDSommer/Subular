f663d900d4ac45a95ab10fc83be2793d23dbdf64
Josh Sommer <JoshDSommer@gmail.com>
2018-07-04 11:07:59 -0400

    updating services to rxjs5

--- libs/core/src/lib/services/subsonic.cached.service.ts
+++ libs/core/src/lib/services/subsonic.cached.service.ts
@@ -1,103 +1,114 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { SubsonicService } from './subsonic.service';
 import { IAlbum } from '../interfaces/album';
 import { IArtist } from '../interfaces/artists';
 import { LOCALSTORAGE_PROVIDER } from '../localstorage.provider';
-import 'rxjs/add/observable/combineLatest';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/switchMap';
+import { map, tap, switchMap } from 'rxjs/operators';
+import { combineLatest, Observable, of } from 'rxjs';
 
 export const SUBULAR_CACHED_ALBUMS = 'subular.cached.albums';
 export const SUBULAR_CACHED_ARTISTS = 'subular.cached.artists';
 
 @Injectable()
 export class SubsonicCachedService {
+  // private artists: IArtist[];
 
-	// private artists: IArtist[];
+  constructor(
+    private subsonic: SubsonicService,
+    private localStorage: LOCALSTORAGE_PROVIDER
+  ) {}
 
-	constructor(private subsonic: SubsonicService, private localStorage: LOCALSTORAGE_PROVIDER) {
+  getCachedData(): Observable<[IArtist[], IAlbum[]]> {
+    return combineLatest(this.getArtists(), this.getAlbums()) as any;
+  }
 
-	}
+  getArtists(): Observable<IArtist[]> {
+    const artists = this.localStorage.getValue(SUBULAR_CACHED_ARTISTS);
+    if (artists) {
+      return new Observable(observer => {
+        observer.next(artists);
+        observer.complete();
+      });
+    }
+    // else rebuild albumDB  and then get it form local storage.
+    return this.buildArtistDatabase().pipe(switchMap(() => this.getArtists()));
+  }
 
+  getAlbums(): Observable<IAlbum[]> {
+    const albums = this.localStorage.getValue(SUBULAR_CACHED_ALBUMS);
+    if (albums) {
+      return new Observable(observer => {
+        observer.next(albums);
+        observer.complete();
+      });
+    }
+    // else rebuild albumDB  and then get it form local storage.
+    return this.buildAlbumDatabase().pipe(switchMap(() => this.getAlbums()));
+  }
 
+  getAlbum(albumId: number) {
+    const albums = this.localStorage.getValue(SUBULAR_CACHED_ALBUMS);
+    const filtered = albums.find(album => album.id == albumId);
 
-	getCachedData(): Observable<[IArtist[], IAlbum[]]> {
-		return Observable.combineLatest(this.getArtists(), this.getAlbums()) as any;
-	}
+    if (filtered) {
+      return new Observable(observer => {
+        observer.next(filtered);
+        observer.complete();
+      });
+    }
+    // else rebuild albumDB  and then get it form local storage.
+    return this.buildAlbumDatabase().pipe(
+      switchMap(() =>
+        this.getAlbums().pipe(
+          map(albumList => albumList.find(album => album.id === albumId))
+        )
+      )
+    );
+  }
 
-	getArtists(): Observable<IArtist[]> {
-		const artists = this.localStorage.getValue(SUBULAR_CACHED_ARTISTS);
-		if (artists) {
-			return new Observable(observer => {
-				observer.next(artists);
-				observer.complete();
-			});
-		}
-		// else rebuild albumDB  and then get it form local storage.
-		return this.buildArtistDatabase().switchMap(() => this.getArtists());
-	}
+  getArtistById(id: number): Observable<IArtist> {
+    return this.getArtists().pipe(
+      map(artists => artists.find(artist => artist.id == id))
+    );
+  }
 
-	getAlbums(): Observable<IAlbum[]> {
-		const albums = this.localStorage.getValue(SUBULAR_CACHED_ALBUMS);
-		if (albums) {
-			return new Observable(observer => {
-				observer.next(albums);
-				observer.complete();
-			});
-		}
-		// else rebuild albumDB  and then get it form local storage.
-		return this.buildAlbumDatabase().switchMap(() => this.getAlbums());
-	}
+  private cleanSubsonicResponse(data: any): string {
+    return JSON.stringify(data).replace('subsonic-response', 'subresp');
+  }
 
-	getAlbum(albumId: number) {
-		const albums = this.localStorage.getValue(SUBULAR_CACHED_ALBUMS);
-		const filtered = albums.find(album => album.id == albumId)
+  private buildAlbumDatabase(
+    albums: IAlbum[] = [],
+    offset: number = 0
+  ): Observable<boolean> {
+    return this.subsonic
+      .subsonicGet('getAlbumList2', '&type=newest&size=500&offset=' + offset)
+      .pipe(
+        map((data: any) => data.subresp.albumList2.album as IAlbum[]),
+        switchMap((data: any) => {
+          albums = [...albums, ...data];
+          if (data.length === 500) {
+            return this.buildAlbumDatabase(albums, offset + 500);
+          } else {
+            this.localStorage.setValue(SUBULAR_CACHED_ALBUMS, albums);
+            return of(true);
+          }
+        })
+      );
+  }
 
-		if (filtered) {
-			return new Observable(observer => {
-				observer.next(filtered);
-				observer.complete();
-			});
-		}
-		// else rebuild albumDB  and then get it form local storage.
-		return this.buildAlbumDatabase().switchMap(() => this.getAlbums().map(albumList => albumList.find(album => album.id === albumId)));
-
-	}
-
-	getArtistById(id: number): Observable<IArtist> {
-		return this.getArtists().map(artists => artists.find(artist => artist.id == id));
-	}
-
-
-
-	private cleanSubsonicResponse(data: any): string {
-		return JSON.stringify(data).replace('subsonic-response', 'subresp');
-	}
-
-	private buildAlbumDatabase(albums: IAlbum[] = [], offset: number = 0): Observable<boolean> {
-		return this.subsonic
-			.subsonicGet('getAlbumList2', '&type=newest&size=500&offset=' + offset)
-			.map(data => data.subresp.albumList2.album as IAlbum[])
-			.switchMap(data => {
-				albums = [...albums, ...data,]
-				if (data.length === 500) {
-					return this.buildAlbumDatabase(albums, offset + 500);
-				} else {
-					this.localStorage.setValue(SUBULAR_CACHED_ALBUMS, albums);
-					return Observable.of(true);
-				}
-			});
-	}
-
-	private buildArtistDatabase(): Observable<any> {
-		return this.subsonic.subsonicGet('getArtists')
-			.map(data => data.subresp.artists.index
-				.map(value => value.artist)
-				.reduce((previous, value) => [...previous, ...value]) as IArtist[]
-			)
-			.do(artists => this.localStorage.setValue(SUBULAR_CACHED_ARTISTS, artists))
-	}
+  private buildArtistDatabase(): Observable<any> {
+    return this.subsonic
+      .subsonicGet('getArtists')
+      .pipe(
+        map(
+          (data: any) =>
+            data.subresp.artists.index
+              .map(value => value.artist)
+              .reduce((previous, value) => [...previous, ...value]) as IArtist[]
+        ),
+        tap(artists =>
+          this.localStorage.setValue(SUBULAR_CACHED_ARTISTS, artists)
+        )
+      );
+  }
 }
--- libs/core/src/lib/subsonic.guard.ts
+++ libs/core/src/lib/subsonic.guard.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
 import { SubsonicService } from './services/subsonic.service';
 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/do';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class SubsonicGuard implements CanActivate {
+  constructor(private subsonic: SubsonicService, private router: Router) {}
 
-	constructor(private subsonic: SubsonicService, private router: Router) {
-
-	}
-
-	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-		return this.subsonic.pingServer().do(authenticated => {
-			if (!authenticated) {
-				this.router.navigate(['/login']);
-			}
-			return authenticated;
-		});
-	}
-
-
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.subsonic.pingServer().pipe(
+      tap(authenticated => {
+        if (!authenticated) {
+          this.router.navigate(['/login']);
+        }
+        return authenticated;
+      })
+    );
+  }
 }
