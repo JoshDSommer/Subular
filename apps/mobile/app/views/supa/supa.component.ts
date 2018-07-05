ccafb1a45812a15c4aadaea4e98a4772e5d490ac
Josh Sommer <JoshDSommer@gmail.com>
2018-07-05 12:10:46 -0400

    adding clear cache to supa

--- apps/mobile/app/views/supa/supa.component.ts
+++ apps/mobile/app/views/supa/supa.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { getString, setString } from 'tns-core-modules/application-settings';
+import { CACHED_SONGS_KEY, CACHED_PLAYLISTS_KEY } from '~/services';
+import { SUBULAR_CACHED_ALBUMS, SUBULAR_CACHED_ARTISTS } from '@Subular/core';
+import { RouterExtensions } from 'nativescript-angular/router';
 
 @Component({
   moduleId: module.id,
@@ -7,7 +11,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./supa.component.css']
 })
 export class SupaComponent implements OnInit {
-  constructor() {}
+  constructor(private router: RouterExtensions) {}
 
   ngOnInit() {}
+
+  clearCachedAlubms() {
+    setString(SUBULAR_CACHED_ALBUMS, '');
+    setString(SUBULAR_CACHED_ARTISTS, '');
+    this.router.navigate(['/login'], { clearHistory: true });
+  }
 }
