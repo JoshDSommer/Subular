240e730cde157a31f7f46d256c7e5b47262c95c0
Josh Sommer <JoshSo@Joshs-MacBook-Pro.local>
2018-07-07 11:36:05 -0400

    moving art assets

--- libs/core/src/lib/services/subsonic.service.ts
+++ libs/core/src/lib/services/subsonic.service.ts
@@ -10,107 +10,134 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/catch';
 @Injectable()
 export class SubsonicService {
+  constructor(
+    private authentication: SubsonicAuthenticationService,
+    private http: HttpClient
+  ) {}
 
-	constructor(private authentication: SubsonicAuthenticationService, private http: HttpClient) { }
+  pingServer(): Observable<boolean> {
+    return this.subsonicGet('ping')
+      .map(data => data.subresp.status === 'ok')
+      .catch(() => of(false));
+  }
 
-	pingServer(): Observable<boolean> {
-		return this.subsonicGet('ping')
-			.map(data => data.subresp.status === 'ok')
-			.catch(() => of(false));
-	}
+  getStreamUrl(id: number): string {
+    return `${this.authentication.getServerURl('stream')}&id=${id}`;
+  }
 
-	getStreamUrl(id: number): string {
-		return `${this.authentication.getServerURl('stream')}&id=${id}`;
-	}
+  getDownloadUrl(id: number): string {
+    return `${this.authentication.getServerURl('download')}&id=${id}`;
+  }
 
-	getDownloadUrl(id: number): string {
-		return `${this.authentication.getServerURl('download')}&id=${id}`;
-	}
+  getHLSStream(id: number) {
+    return `${this.authentication.getHLSURl(id)}`;
+  }
 
+  getPlaylists(): Observable<IPlaylists> {
+    return this.subsonicGet('getPlaylists').map(
+      data => data.subresp.playlists.playlist
+    );
+  }
 
-	getHLSStream(id: number) {
+  getRecentAdditions(): Observable<IAlbum[]> {
+    return this.subsonicGet('getAlbumList2', '&type=newest&size=50').map(
+      response => response.subresp.albumList2.album
+    );
+  }
 
-		return `${this.authentication.getHLSURl(id)}`;
-	}
+  getPlaylist(id: number): Observable<IPlaylist> {
+    return this.subsonicGet('getPlaylist', `&id=${id}`)
+      .map(data => data.subresp.playlist)
+      .map(playlist => {
+        playlist.entry = playlist.entry.map((song: ISong) => {
+          const measuredTime = new Date(song.duration * 1000);
+          const timeWithoutHours = measuredTime.toISOString().substr(14, 5);
+          const timeWithHours = measuredTime.toISOString().substr(11, 8);
+          song.time = timeWithHours.startsWith('00:')
+            ? timeWithoutHours
+            : timeWithHours;
+          return song;
+        });
+        return playlist;
+      });
+  }
 
-	getPlaylists(): Observable<IPlaylists> {
-		return this.subsonicGet('getPlaylists').map(data => data.subresp.playlists.playlist);
-	}
+  createPlaylist(name: string): Observable<number> {
+    return this.subsonicGet('createPlaylist', `&name=${name}`);
+  }
 
-	getRecentAdditions(): Observable<IAlbum[]> {
-		return this.subsonicGet('getAlbumList2', '&type=newest&size=50')
-			.map(response => response.subresp.albumList2.album);
-	}
+  addSongToPlaylist(song: ISong, playlistId: number) {
+    return this.subsonicGet(
+      'updatePlaylist',
+      `&playlistId=${playlistId}&songIdToAdd=${song.id}`
+    );
+  }
 
-	getPlaylist(id: number): Observable<IPlaylist> {
-		return this.subsonicGet('getPlaylist', `&id=${id}`)
-			.map(data => data.subresp.playlist)
-			.map(playlist => {
-				playlist.entry = playlist.entry.map((song: ISong) => {
-					const measuredTime = new Date(song.duration * 1000);
-					const timeWithoutHours = measuredTime.toISOString().substr(14, 5);
-					const timeWithHours = measuredTime.toISOString().substr(11, 8);
-					song.time = timeWithHours.startsWith('00:') ? timeWithoutHours : timeWithHours;
-					return song;
-				});
-				return playlist;
-			});
-	}
+  changePlaylistName(name: string, playlistId: number) {
+    return this.subsonicGet(
+      'updatePlaylist',
+      `&playlistId=${playlistId}&name=${name}`
+    );
+  }
 
-	createPlaylist(name: string): Observable<number> {
-		return this.subsonicGet('createPlaylist', `&name=${name}`);
-	}
+  removeSongFromPlaylist(songIndexToRemove, playlistId: number) {
+    return this.subsonicGet(
+      'updatePlaylist',
+      `&playlistId=${playlistId}&songIndexToRemove=${songIndexToRemove}`
+    );
+  }
 
-	addSongToPlaylist(song: ISong, playlistId: number) {
-		return this.subsonicGet('updatePlaylist', `&playlistId=${playlistId}&songIdToAdd=${song.id}`);
-	}
+  getArtistInfo(artistId: number) {
+    return this.subsonicGet('getArtistInfo2', `&id=${artistId}`);
+  }
 
-	changePlaylistName(name: string, playlistId: number) {
-		return this.subsonicGet('updatePlaylist', `&playlistId=${playlistId}&name=${name}`);
-	}
+  getSongs(albumId: number): Observable<ISong[]> {
+    return this.subsonicGet('getAlbum', `&id=${albumId}`)
+      .map(data => data.subresp.album.song)
+      .map(songs => {
+        return songs.map((song: ISong) => {
+          const measuredTime = new Date(song.duration * 1000);
+          const timeWithoutHours = measuredTime.toISOString().substr(14, 5);
+          const timeWithHours = measuredTime.toISOString().substr(11, 8);
+          song.time = timeWithHours.startsWith('00:')
+            ? timeWithoutHours
+            : timeWithHours;
+          return song;
+        });
+      });
+  }
 
-	removeSongFromPlaylist(songIndexToRemove, playlistId: number) {
-		return this.subsonicGet('updatePlaylist', `&playlistId=${playlistId}&songIndexToRemove=${songIndexToRemove}`);
-	}
+  getTopSongs(artistName): Observable<ISong[]> {
+    return this.subsonicGet('getTopSongs', `&artist=${artistName}`);
+  }
 
-	getSongs(albumId: number): Observable<ISong[]> {
-		return this.subsonicGet('getAlbum', `&id=${albumId}`)
-			.map(data => data.subresp.album.song)
-			.map(songs => {
-				return songs.map((song: ISong) => {
-					const measuredTime = new Date(song.duration * 1000);
-					const timeWithoutHours = measuredTime.toISOString().substr(14, 5);
-					const timeWithHours = measuredTime.toISOString().substr(11, 8);
-					song.time = timeWithHours.startsWith('00:') ? timeWithoutHours : timeWithHours;
-					return song;
-				});
-			});
-	}
+  subsonicGetCoverUrl(id: number, size = 274): string {
+    return (
+      this.authentication.getServerURl('getCoverArt') + `&id=${id}&size=${274}`
+    );
+  }
 
-	getTopSongs(artistName): Observable<ISong[]> {
-		return this.subsonicGet('getTopSongs', `&artist=${artistName}`);
-	}
+  starSong(id: number) {
+    return this.subsonicGet('star', `&id=${id}`);
+  }
+  unStarSong(id: number) {
+    return this.subsonicGet('unstar', `&id=${id}`);
+  }
 
-	subsonicGetCoverUrl(id: number, size = 274): string {
-		return this.authentication.getServerURl('getCoverArt') + `&id=${id}&size=${274}`;
-	}
+  subsonicGet(method: string);
+  subsonicGet(method: string, additionalParams: string);
+  subsonicGet(method: string, additionalParams?: string) {
+    const url = additionalParams
+      ? this.authentication.getServerURl(method) + additionalParams
+      : this.authentication.getServerURl(method);
+    if (url === '' || url === additionalParams) {
+      return of(false);
+    }
 
-	starSong(id: number) {
-		return this.subsonicGet('star', `&id=${id}`);
-	}
-	unStarSong(id: number) {
-		return this.subsonicGet('unstar', `&id=${id}`);
-	}
-
-	subsonicGet(method: string);
-	subsonicGet(method: string, additionalParams: string);
-	subsonicGet(method: string, additionalParams?: string) {
-		const url = additionalParams ? this.authentication.getServerURl(method) + additionalParams : this.authentication.getServerURl(method);
-		if (url === '' || url === additionalParams) {
-			return of(false);
-		}
-
-		return this.http.get(url)
-			.map(data => JSON.parse(JSON.stringify(data).replace('subsonic-response', 'subresp')));
-	}
+    return this.http
+      .get(url)
+      .map(data =>
+        JSON.parse(JSON.stringify(data).replace('subsonic-response', 'subresp'))
+      );
+  }
 }
