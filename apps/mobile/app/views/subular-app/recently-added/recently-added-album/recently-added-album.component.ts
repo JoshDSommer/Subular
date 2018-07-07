edc917b0c4d54f5a1dbc5800b5403008eef7a0c6
Josh Sommer <JoshDSommer@gmail.com>
2018-07-06 22:14:12 -0400

    refactoring view to have on push child components

---
+++ apps/mobile/app/views/subular-app/recently-added/recently-added-album/recently-added-album.component.ts
@@ -0,0 +1,36 @@
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input
+} from '@angular/core';
+import { SubularMobileService } from '~/services';
+import { IAlbum } from '@Subular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'recently-added-album',
+  templateUrl: './recently-added-album.component.html',
+  styleUrls: ['./recently-added-album.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RecentlyAddedAlbumComponent implements OnInit {
+  @Input() album: IAlbum;
+  @Input() width: number;
+  @Input() margin: number;
+
+  get imageHeightWidth() {
+    return this.width;
+  }
+  get imageSideMargins() {
+    return this.margin;
+  }
+
+  constructor(public subular: SubularMobileService) {}
+
+  ngOnInit() {}
+
+  getCoverArt(song) {
+    return this.subular.subsonicGetAlbumCoverUrl(song);
+  }
+}
