f8d8fa36fc9d44ae51d54c4953fac75f7406e848
Josh Sommer <JoshSo@Joshs-MacBook-Pro.local>
2018-10-27 08:21:16 -0400

    adding new mobile app
    bassed off of tab navigation template

---
+++ apps/mobile/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+
+import { BrowseComponent } from "./browse/browse.component";
+import { HomeComponent } from "./home/home.component";
+import { ItemDetailComponent } from "./item-detail/item-detail.component";
+import { SearchComponent } from "./search/search.component";
+
+export const COMPONENTS = [BrowseComponent, HomeComponent, ItemDetailComponent, SearchComponent];
+
+const routes: Routes = [
+    { path: "", redirectTo: "/(homeTab:home//browseTab:browse//searchTab:search)", pathMatch: "full" },
+
+    { path: "home", component: HomeComponent, outlet: "homeTab" },
+    { path: "browse", component: BrowseComponent, outlet: "browseTab" },
+    { path: "search", component: SearchComponent, outlet: "searchTab" },
+
+    { path: "item/:id", component: ItemDetailComponent, outlet: "homeTab" }
+];
+
+@NgModule({
+    imports: [NativeScriptRouterModule.forRoot(routes)],
+    exports: [NativeScriptRouterModule]
+})
+export class AppRoutingModule { }
---
+++ apps/mobile/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+
+import { AppRoutingModule, COMPONENTS } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        AppRoutingModule,
+        CoreModule
+    ],
+    declarations: [
+        AppComponent,
+        ...COMPONENTS
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class AppModule { }
