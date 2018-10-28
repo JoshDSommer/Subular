424c2f8e195067e36ff23af79778977d863eeab4
Josh Sommer <JoshSo@Joshs-MacBook-Pro.local>
2018-10-28 08:05:13 -0400

    changing app to basic default app

--- apps/mobile/src/app/app.component.ts
+++ apps/mobile/src/app/app.component.ts
@@ -1,25 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { isAndroid } from "tns-core-modules/platform";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "ns-app",
     moduleId: module.id,
-    templateUrl: "app.component.html",
-    styleUrls: ["./app.component.scss"]
+    templateUrl: "./app.component.html",
 })
-export class AppComponent implements OnInit {
-
-    constructor() {
-        // Use the component constructor to inject providers.
-    }
-
-    ngOnInit(): void {
-        // Init your component properties here.
-    }
-
-    getIconSource(icon: string): string {
-        const iconPrefix = isAndroid ? "res://" : "res://tabIcons/";
-
-        return iconPrefix + icon;
-    }
-}
+export class AppComponent { }
--- apps/mobile/src/app/app.module.ts
+++ apps/mobile/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
-import { AppRoutingModule, COMPONENTS } from "./app-routing.module";
+import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
-import { CoreModule } from "./core/core.module";
+
+import { ItemService } from "./item/item.service";
+import { ItemsComponent } from "./item/items.component";
+import { ItemDetailComponent } from "./item/item-detail.component";
+
+// Uncomment and add to NgModule imports if you need to use two-way binding
+// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+
+// Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
+// import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
 
 @NgModule({
     bootstrap: [
@@ -11,15 +19,21 @@ import { CoreModule } from "./core/core.module";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule,
-        CoreModule
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
-        ...COMPONENTS
+        ItemsComponent,
+        ItemDetailComponent
+    ],
+    providers: [
+        ItemService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
 })
+/*
+Pass your application module to the bootstrapModule function located in main.ts to start your app
+*/
 export class AppModule { }
