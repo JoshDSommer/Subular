01f3ce3607826d29ac83cb52f9a17ed5e6a4954e
Josh Sommer <JoshSo@Joshs-MacBook-Pro.local>
2018-11-04 10:17:24 -0500

    Adding Subluar-web app.
    new angular web app includes, cypress and jest, this is for testing.

---
+++ apps/subular-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Subular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'subular-web';
+}
