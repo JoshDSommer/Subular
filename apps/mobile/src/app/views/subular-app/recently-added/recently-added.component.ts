2ae11968f32754fec2d59a1ee5cf87f512587697
Josh Sommer <JoshSo@Joshs-MacBook-Pro.local>
2018-12-01 14:39:00 -0500

    disabling ng-container loading screen.
    this seems to not be happy with the route animations. and with my skeleton strategy it is never seen any more. so there is no point.

--- apps/mobile/src/app/views/subular-app/recently-added/recently-added.component.ts
+++ apps/mobile/src/app/views/subular-app/recently-added/recently-added.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ChangeDetectorRef
+} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { IAlbum } from '@Subular/core';
 import { RouterExtensions } from 'nativescript-angular/router';
@@ -9,8 +14,7 @@ import {
 import { SubularMobileService } from '../../../services/subularMobile.service';
 import { screen, isIOS } from 'tns-core-modules/platform/platform';
 import { Observable } from 'rxjs/Observable';
-import { tap, map, switchMap, delay, combineAll } from 'rxjs/operators';
-import { of, concat, zip } from 'rxjs';
+import { tap, map, delay } from 'rxjs/operators';
 import { popIn } from '../../../pipes/popin.pipe';
 import { ItemEventData } from 'tns-core-modules/ui/list-view/list-view';
 
@@ -19,21 +23,22 @@ import { ItemEventData } from 'tns-core-modules/ui/list-view/list-view';
   selector: 'recently-added',
   templateUrl: './recently-added.component.html'
 })
-export class RecentlyAddedComponent implements OnInit {
+export class RecentlyAddedComponent implements OnInit, AfterViewInit {
   albums$: Observable<IAlbum[][]>;
 
-  SPIN_ANIMATION = SPIN_ANIMATION;
-  SLIDE_RIGHT_ANIMATION = SLIDE_RIGHT_ANIMATION;
+  // SPIN_ANIMATION = SPIN_ANIMATION;
+  // SLIDE_RIGHT_ANIMATION = SLIDE_RIGHT_ANIMATION;
 
   imageHeightWidth = (screen.mainScreen.widthDIPs / 12) * 5;
   imageSideMargins = screen.mainScreen.widthDIPs / 18;
-  albums: IAlbum[][];
+  albums: IAlbum[][] = [];
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private nsRouter: RouterExtensions,
-    public subular: SubularMobileService
+    public subular: SubularMobileService,
+    private ref: ChangeDetectorRef
   ) {}
 
   getAlbumsText(albums: IAlbum[]) {
@@ -47,7 +52,9 @@ export class RecentlyAddedComponent implements OnInit {
     this.albums$ = this.subular.getRecentAdditions().pipe(
       map(albums => smashArray<IAlbum>(albums)),
       popIn,
-      tap(albums => (this.albums = albums))
+      tap(albums => (this.albums = albums)),
+      tap(console.log),
+      tap(() => this.ref.markForCheck())
     );
   }
 
@@ -56,6 +63,10 @@ export class RecentlyAddedComponent implements OnInit {
       const iosCell = args.ios;
       iosCell.selectionStyle = UITableViewCellSelectionStyle.None;
     }
+  }
+
+  ngAfterViewInit() {
+    this.albums$.subscribe();
   }
 }
 
